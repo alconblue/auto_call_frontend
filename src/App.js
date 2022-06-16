8d79a6c188c971e11ca5aae0b4712930bc4041a4
Alconblue <zoebmithaiwala1@gmail.com>
2022-06-16 17:00:57 +0530

    Update the index page to a form for input number and message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [phone, setPhone] = useState("");
+  const [status, setStatus] = useState("");
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("/call", {method: "POST", body: JSON.stringify({message: message, ph_no: phone,}),});
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setMessage("");
+        setPhone("");
+        setStatus("Call added successfully");
+      } else {
+        setStatus("Some error occurred");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={message} placeholder="Message" onChange={(e) => setMessage(e.target.value)}/>
+        <input type="text" value={phone} placeholder="Phone Number" onChange={(e) => setPhone(e.target.value)}/>
+        <button type="submit">Call</button>
+        </form>
+        <div className="status">{status ? <p>{status}</p> : null}</div>
     </div>
   );
 }
